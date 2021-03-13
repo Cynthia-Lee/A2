4e440a9da99ace51bbd61001ff5d5c4cf5517efb
Cynthia Lee <cynth.lee23@gmail.com>
2021-03-12 21:03:01 -0500

    finished plain dfsb

--- dfsb.py
+++ dfsb.py
@@ -5,6 +5,7 @@ import sys
 # Lecture slides
 # http://pages.cs.wisc.edu/~bgibson/cs540/handouts/csp.pdf
 # https://en.wikipedia.org/wiki/Min-conflicts_algorithm
+# http://aimacode.github.io/aima-java/aima3e/javadoc/aima-core/aima/core/search/csp/BacktrackingStrategy.html
 # -------------------------------------------------------
 
 class CSP:
@@ -47,10 +48,10 @@ def input_to_csp(file):
     print("constraints", constraints)
     return csp
 
-def plain_select_unassigned_variable(variables, assignment, csp):
+def plain_select_unassigned_variable(csp):
     # variables is unassigned items
     # This default implementation just selects the first in the ordered list of variables provided by the CSP.
-    var = variables[0]
+    var = csp.variables[0]
     csp.variables.remove(var)
     return var
 
@@ -62,7 +63,7 @@ def plain_order_domain_values(var, assignment, csp):
         domain_values.append(k)
     return domain_values
 
-def consistent(var, value, assignment, csp):
+def consistent(var, value, assignment, csp): # adjacent nodes cannot have the same color - check is consistent with the current assignments
     for constraint in csp.constraints:
         if (var in constraint):
             neighbor = constraint[constraint.index(var)-1]
@@ -79,156 +80,16 @@ def plain_backtracking_search(csp):
 def plain_recursive_backtracking(assignment, csp): # returns solution or failure
     if (is_complete(assignment, csp)): # if assignment is complete, return assignment (like goal test)
         return assignment
-    var = plain_select_unassigned_variable(csp.variables, assignment, csp) # var <- select_unassigned_variable(variables[csp],assignment,csp)
-    print("var", var)
-    for value in plain_order_domain_values(var, assignment, csp):
-        # given the variable (var) that we have, explore all possible values that you can assign
+    var = plain_select_unassigned_variable(csp) # var <- select_unassigned_variable(variables[csp],assignment,csp)
+    for value in plain_order_domain_values(var, assignment, csp): # given the variable (var) that we have, explore all possible values that you can assign
         if consistent(var, value, assignment, csp): # if value is consistent with assignment given constraints[csp] then
-            # adjacent nodes cannot have the same color - check is consistent with the current assignments
             assignment[var] = value # add {var = value} to assignment
-            print("a", assignment)
             result = plain_recursive_backtracking(assignment, csp)
             if (result): # if result not equal failure then return result
                 return result
             assignment.pop(var, None) # remove {var = value} from assignment
     return False
 
-'''
-# writes the solution path array to the output file
-def write_output(path, file):
-    output = ",".join(path)
-    f = open(file, "w")
-    for ch in output:
-        f.write(ch)
-    f.close()
-    return True
-
-@dataclass(order=True)
-class PrioritizedItem:
-    priority: int
-    item: Any=field(compare=False)
-
-class Node:
-    def __init__(self, problem, parent=None, action=None):
-        self.problem = problem
-        self.parent = parent
-        self.action = action
-        self.state = problem.state
-        # g(n)
-        if (self.parent != None):
-            # cost of parent's path + distance from between parent and node
-            self.g = parent.g + 1 
-        else:
-            self.g = 0
-        # f(n) = g(n) + h(n)
-
-def reconstruct_path(node):
-    solution = []
-    ptr = node
-    while(ptr.parent!=None):
-        solution.insert(0, ptr.action)
-        ptr = ptr.parent
-    # print("Depth", len(solution))
-    return solution
-
-def a_star(problem, h):
-    # frontier = priority_queue() # sort frontier on expected path cost
-    frontier = queue.PriorityQueue()
-    # frontier = frontier + make-node(start)
-    start = Node(problem)
-    frontier.put(PrioritizedItem(0, start))
-    explored = []
-
-    # f(n) = g(n) + h(n)
-    # g(n) = cost of path from start to n so far (dist from root)
-    # h(n) = estimated cost from n to G (end node)
-
-    # start has no parent
-    # start.g = 0
-    start.h = h(start.state)
-    start.f = start.g + start.h
-
-    # while not frontier.isempty():
-    while not frontier.empty():
-        # current <- pop(frontier) # i.e., the top of the queue
-        current = frontier.get().item
-        # if goal-test(current) return success # goal test when node expands
-        if problem.goal_test(current.state):
-            # print("Number of states explored:", len(explored)+1, "ANSWER:", reconstruct_path(current))
-            return reconstruct_path(current) 
-        # if current not in explored:
-        if not current in explored:
-            # explored <- explored + current.state
-            explored.append(current.state)
-            # for each action in current.actions():
-            for action in problem.state_actions(current.state): # neighbors/sucessors
-                # new <- action(current.state)
-                new = problem.change_state(current.state, action) # new problem
-                # new-node <- make-node(new, current, action)
-                new_node = Node(new, current, action) # neighbor
-                # new_node.g = new_node.parent.g + 1 
-                new_node.h = h(new_node.state)
-                new_node.f = new_node.g + new_node.h
-                
-                if (not new_node.state in explored):
-                    # frontier = frontier + new-node
-                    frontier.put(PrioritizedItem(new_node.f, new_node))
-    return False
-
-counter = 1
-
-def recursive_best_first_search(problem, h):
-    global counter
-    counter = 1
-    # solution, fvalue <- rbfs(problem, node(problem.initial), inf, h)
-    start = Node(problem)
-    start.h = h(start.state)
-    start.f = start.g + start.h
-    solution = rbfs(problem, start, float("inf"), h)
-    return solution
-
-def rbfs(problem, node, f_limit, h):
-    # returns solution or failure, and a new f-cost limit
-    if (problem.goal_test(node.state)):
-        return reconstruct_path(node)
-    sucessors = []
-    for action in problem.state_actions(node.state, node.action): # neighbors/sucessors
-        # add child_node(problem, node, action) into sucessors
-        new = problem.change_state(node.state, action) # new problem
-        child_node = Node(new, node, action) # neighbor
-        child_node.h = h(child_node.state)
-        child_node.f = child_node.g + child_node.h
-        sucessors.append(child_node)
-    if (not sucessors): # if successors is empty
-        # return failure, infinity
-        return (False, float("inf"))
-    for s in sucessors:
-        # update f with value from previous search if any
-        s.f = max(s.g + s.h, node.f)
-    # loop do
-    while True:
-        # best <- lowest f-value in successors
-        best = sucessors[0]
-        alternative = best
-        for s in sucessors:
-            if (s.f <= best.f):
-                alternative = best
-                best = s
-            elif (s.f <= alternative.f):
-                alternative = s
-        # if best.f > f-limit then return failure, best.f
-        if (best.f > f_limit):
-            return (False, best.f)
-        # alternative <- the second lowest f-value among successors
-        # result, best.f <- RBFS(problem, best, min(f-limit,alternative))
-        global counter
-        counter += 1
-        result = rbfs(problem, best, min(f_limit, alternative.f), h)
-        best.f = result[1]
-        # if result not = failure then return result
-        if (result[0]):
-            return result
-'''
 # -------------------------------------------------------
 ### Main class ###
 
@@ -244,5 +105,4 @@ if __name__ == '__main__':
     output = (sys.argv[2]) # OUTPUT FILE PATH
     mode = (sys.argv[3]) # MODE FLAG
     
-    test = input_to_csp(input)
-    print(plain_backtracking_search(test))
+    print(plain_backtracking_search(input_to_csp(input)))
