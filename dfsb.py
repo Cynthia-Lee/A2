d583366a1bad00a1a6c1348e9b01432e1a3dea75
Cynthia Lee <cynth.lee23@gmail.com>
2021-03-12 22:22:35 -0500

    coded mrv

--- dfsb.py
+++ dfsb.py
@@ -48,12 +48,15 @@ def input_to_csp(file):
     print("constraints", constraints)
     return csp
 
-def plain_select_unassigned_variable(csp):
+def select_unassigned_variable(csp):
     # variables is unassigned items
     # This default implementation just selects the first in the ordered list of variables provided by the CSP.
     var = csp.variables[0]
     csp.variables.remove(var)
     return var
+
+# -------------------------------------------------------
+### Plain DFS-B ###
 
 def plain_order_domain_values(var, assignment, csp):
     # Primitive operation, ordering the domain values of the specified variable. 
@@ -63,7 +66,7 @@ def plain_order_domain_values(var, assignment, csp):
         domain_values.append(k)
     return domain_values
 
-def consistent(var, value, assignment, csp): # adjacent nodes cannot have the same color - check is consistent with the current assignments
+def plain_consistent(var, value, assignment, csp): # adjacent nodes cannot have the same color - check is consistent with the current assignments
     for constraint in csp.constraints:
         if (var in constraint):
             neighbor = constraint[constraint.index(var)-1]
@@ -80,14 +83,73 @@ def plain_backtracking_search(csp):
 def plain_recursive_backtracking(assignment, csp): # returns solution or failure
     if (is_complete(assignment, csp)): # if assignment is complete, return assignment (like goal test)
         return assignment
-    var = plain_select_unassigned_variable(csp) # var <- select_unassigned_variable(variables[csp],assignment,csp)
+    var = select_unassigned_variable(csp) # var <- select_unassigned_variable(variables[csp],assignment,csp)
     for value in plain_order_domain_values(var, assignment, csp): # given the variable (var) that we have, explore all possible values that you can assign
-        if consistent(var, value, assignment, csp): # if value is consistent with assignment given constraints[csp] then
+        if plain_consistent(var, value, assignment, csp): # if value is consistent with assignment given constraints[csp] then
             assignment[var] = value # add {var = value} to assignment
             result = plain_recursive_backtracking(assignment, csp)
             if (result): # if result not equal failure then return result
                 return result
             assignment.pop(var, None) # remove {var = value} from assignment
+    return False
+
+# -------------------------------------------------------
+### Improved DFS-B ###
+
+# DFS-B with variable, value ordering + AC3 for constraint propagation
+
+# Variable (select-unassigned-variable)
+    # Most constrained variable, Minimum remaining values (MRV) heuristic 
+# Value ordering (order-domain-values)
+    # Least constraining value, (LCV) heuristic
+# Constraint propagation (is-consistent) (inference)
+    # AC3, arc consistency
+    # Forward checking
+
+def mrv(constraints):
+    # most constrained variable, use minimum remaining values (MRV) degree heuristic
+    # choose the variable with the fewest legal values
+    # choose the variable with the most constraints on remaining variables
+    counter = {}
+    for constraint in constraints:
+        u = constraint[0]
+        v = constraint [1]
+        if (u not in counter):
+            counter[u] = 0
+        if (v not in counter):
+            counter[v] = 0
+        counter[u] += 1
+        counter[v] += 1
+    ordered_variables = sorted(counter, key=counter.get)
+    ordered_variables = ordered_variables[::-1]
+    return ordered_variables
+
+def improved_order_domain_values(var, assignment, csp):
+    # least constraining value, use LCV
+    print()
+
+def improved_consistent(var, value, assignment, csp):
+    # use AC3
+    # use forward checking
+    print()
+
+def improved_backtracking_search(csp):
+    csp.variables = mrv(csp.constraints) # enable mrv for select_unassigned_variable
+    return improved_recursive_backtracking({}, csp)
+
+def improved_recursive_backtracking(assignment, csp):
+    if (is_complete(assignment, csp)): # if assignment is complete, return assignment (like goal test)
+        return assignment
+    var = select_unassigned_variable(csp) # var <- select_unassigned_variable(variables[csp],assignment,csp)
+    '''
+    for value in improved_order_domain_values(var, assignment, csp): # given the variable (var) that we have, explore all possible values that you can assign
+        if improved_consistent(var, value, assignment, csp): # if value is consistent with assignment given constraints[csp] then
+            assignment[var] = value # add {var = value} to assignment
+            result = improved_recursive_backtracking(assignment, csp)
+            if (result): # if result not equal failure then return result
+                return result
+            assignment.pop(var, None) # remove {var = value} from assignment
+    '''
     return False
 
 # -------------------------------------------------------
@@ -105,4 +167,5 @@ if __name__ == '__main__':
     output = (sys.argv[2]) # OUTPUT FILE PATH
     mode = (sys.argv[3]) # MODE FLAG
     
-    print(plain_backtracking_search(input_to_csp(input)))
+    # print(plain_backtracking_search(input_to_csp(input)))
+    print(improved_backtracking_search(input_to_csp(input)))
